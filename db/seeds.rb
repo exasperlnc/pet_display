4ad52145063a34695c7311c471ec65624c0381da
Logan Cole <emtlogancole@gmail.com>
2024-08-07 16:25:24 -0500

    setup tailwind

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,6 @@
+require 'faker'
+require 'factory_bot_rails'
+
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +8,10 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+10.times do
+  user = FactoryBot.create(:user)
+  3.times do
+    FactoryBot.create(:pet, user: user)
+  end
+end
